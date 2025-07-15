c4aa5113990033e0d2b25daa969a4bea0b0109b7
JRReyesM <jreyesm@proton.me>
2025-07-15 12:39:03 +0200

    Javascript interactions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://tutorial-blog-tango.netlify.app"
+  site: "https://tutorial-blog-tango.netlify.app",
+  integrations: [preact()]
 });
